370868ba9c37837a0d7ef130066afa69169ae6fa
Mahamadou <mmohmo@yahoo.com>
2023-09-19 16:50:41 -0400

    carousel added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { Footer } from './components/Footer.js';
 import { ShoppingCart } from './components/ShoppingCart.js';
 import { ContactPage } from './pages/ContactPage.js';
 import { RegisterFom } from './components/signUpForm/RegisterForm.js';
+import { HomePage } from './pages/HomePage.js';
 
 
 function App(props) {
@@ -35,6 +36,7 @@ function App(props) {
       ): (
         <>
           <Routes>
+          <Route path="/" element={<HomePage/>} />
             <Route path="/about" element={<AboutPage/>} />
             <Route path="/account" element={<AuthPage/>} />
             <Route path="/shop" element={<ShopPage/>}/>
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import { CarouselEl } from "../components/carousel/Carousel"
+
+export const HomePage = () => {
+    return(
+        <div
+            
+        >
+            <h2>Home Page</h2>
+            <CarouselEl/>
+        </div>
+    )
+}
