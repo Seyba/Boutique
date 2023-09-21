e77d77e618cb8c337431a938a333d7cdda4895f5
Mahamadou <mmohmo@yahoo.com>
2023-09-20 22:58:29 -0400

    protected routes added

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import { ShoppingCart } from './components/ShoppingCart.js';
 import { ContactPage } from './pages/ContactPage.js';
 import { RegisterFom } from './components/signUpForm/RegisterForm.js';
 import { HomePage } from './pages/HomePage.js';
+import { ProtectedRoutes } from './components/ProtectedRoutes.js';
 
 
 function App(props) {
@@ -22,37 +23,52 @@ function App(props) {
   console.log(user)
   return (
     <BoutiqueContext.Provider value={{user, setUser}}>
-    <main>
-      <Navbar2/>
-      { user? (
-        <>
-          <Navbar user={user} setUser={setUser}/>
-          
+      <main>
+        <Navbar2/>
           <Routes>
-            <Route path="/orders" element={<OrderHistoryPage/>} />
-            <Route path="/orders/new" element={<NewOrderPage/>} />
-          </Routes>
-        </>
-      ): (
-        <>
-          <Routes>
-          <Route path="/" element={<HomePage/>} />
+            <Route element={<ProtectedRoutes/>}>
+              <Route path="/orders" element={<OrderHistoryPage/>}/>
+              <Route path="/orders/new" element={<NewOrderPage/>} />
+            </Route>
+            <Route path="/" element={<HomePage/>} />
             <Route path="/about" element={<AboutPage/>} />
             <Route path="/account" element={<AuthPage/>} />
-            <Route path="/shop" element={<ShopPage/>}/>
             <Route path="/shop/cart" element={<ShoppingCart/>}/>
+            <Route path="/shop" element={<ShopPage/>}/>
             <Route path="/contact" element={<ContactPage/>}/>
             <Route path="/register" element={<RegisterFom/>}/>
           </Routes>
-          {/* <AuthPage user={user} setUser={setUser}/> */}
-        </>
-         
-      )}
-      <Footer/>
-    </main>
+        <Footer/>
+      </main>
     </BoutiqueContext.Provider>
     
   );
 }
 
 export default App;
+
+
+// { user? (
+//   <>
+//     {/* <Navbar user={user} setUser={setUser}/> */}
+    
+//     <Routes>
+//       <Route path="/orders" element={<OrderHistoryPage/>} />
+//       <Route path="/orders/new" element={<NewOrderPage/>} />
+//     </Routes>
+//   </>
+//   ): (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<HomePage/>} />
+//         <Route path="/about" element={<AboutPage/>} />
+//         <Route path="/account" element={<AuthPage/>} />
+//         <Route path="/shop" element={<ShopPage/>}/>
+//         <Route path="/shop/cart" element={<ShoppingCart/>}/>
+//         <Route path="/contact" element={<ContactPage/>}/>
+//         <Route path="/register" element={<RegisterFom/>}/>
+//       </Routes>
+//       {/* <AuthPage user={user} setUser={setUser}/> */}
+//     </>
+//   )
+// }
---
+++ src/components/ProtectedRoutes.js
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import { useContext } from 'react'
+import { BoutiqueContext } from '../context/boutiqueContext'
+
+export const ProtectedRoutes = () => {
+    const ctx = useContext(BoutiqueContext)
+    const { user } = ctx
+    
+    return user? <Outlet/> :<Navigate to="/"/>
+}
