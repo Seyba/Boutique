216574017ad8b8ee3a13f7ed66bfbe357ab68ed5
Mahamadou <mmohmo@yahoo.com>
2023-11-04 20:43:30 -0400

    product rating route added

--- routes/api/product.js
+++ routes/api/product.js
@@ -9,6 +9,7 @@ router.get('/', prodCtr.getAllProducts);
 // GET /api/products/:id
 router.get('/:id', prodCtr.getProdById);
 router.post('/', ensureLoggedIn, isAdmin, prodCtr.createProduct)
+router.put('/rating', ensureLoggedIn, prodCtr.rating)
 router.put('/wishlist', ensureLoggedIn, prodCtr.addProdToWishlist)
 router.put('/:id', ensureLoggedIn, isAdmin, prodCtr.updateProduct)
 router.delete('/:id', ensureLoggedIn, isAdmin, prodCtr.deleteProduct)
